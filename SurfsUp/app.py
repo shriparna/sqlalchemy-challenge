7ae878b9141d462037cad94e46a790fe15d6d235
shriparna <s_v_kamat@yahoo.com>
2023-05-08 12:56:11 -0700

    Moved under SurfsUp

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,19 +1,26 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("Resources/sqlite:///titanic.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Passenger = Base.classes.passenger
 
 # Create our session (link) from Python to the DB
 
@@ -21,10 +28,15 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
